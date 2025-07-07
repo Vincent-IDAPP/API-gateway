7a5b83edafa5fd2f8debb536fdf28011c81c8b57
Vincent Sechi <vincentsechi@MacBook-Pro-de-Vincent.local>
2025-07-07 15:03:11 +0200

    gitignore updates and .env, authcontroller no longer use findOne() method

--- auth-service/controllers/authController.js
+++ auth-service/controllers/authController.js
@@ -5,14 +5,19 @@ const jwt = require('jsonwebtoken');
 exports.register = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const existing = await User.findOne({ email });
-    if (existing) return res.status(400).json({ message: 'Email déjà utilisé' });
 
     const hashed = await bcrypt.hash(password, 10);
     const newUser = new User({ email, password: hashed });
+
     await newUser.save();
+
     res.status(201).json({ message: 'Utilisateur créé' });
   } catch (err) {
+    // Gestion des erreurs mongoose-unique-validator
+    if (err.name === 'ValidationError' && err.errors.email?.kind === 'unique') {
+      return res.status(400).json({ message: 'Email déjà utilisé' });
+    }
+
     res.status(500).json({ message: 'Erreur serveur', error: err });
   }
 };
@@ -20,6 +25,7 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
+
     const user = await User.findOne({ email });
     if (!user) return res.status(404).json({ message: 'Utilisateur non trouvé' });
 
@@ -27,6 +33,7 @@ exports.login = async (req, res) => {
     if (!isMatch) return res.status(401).json({ message: 'Mot de passe incorrect' });
 
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
     res.json({ token });
   } catch (err) {
     res.status(500).json({ message: 'Erreur serveur', error: err });
