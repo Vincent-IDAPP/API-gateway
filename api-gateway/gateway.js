518a38edeb2b805e2d1f4105fb22bd790d150c38
Vincent Sechi <vincentsechi@MacBook-Pro-de-Vincent.local>
2025-07-07 14:52:53 +0200

    proxy library update, gitignore and .env

--- api-gateway/gateway.js
+++ api-gateway/gateway.js
@@ -1,14 +1,12 @@
 require('dotenv').config();
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = require('express-http-proxy');
 
 const app = express();
 const PORT = process.env.GATEWAY_PORT || 3000;
 
-app.use('/auth', createProxyMiddleware({
-  target: 'http://localhost:5001',
-  changeOrigin: true
-}));
+// Proxy vers le service d'auth
+app.use('/', proxy('http://localhost:5001'));
 
 app.listen(PORT, () => {
   console.log(`API Gateway en écoute sur le port ${PORT}`);
